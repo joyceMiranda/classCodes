1e092188b3a64d13b1df917d70d147cb653d4ab5
joyceMiranda <mds.joyce@gmail.com>
2019-08-20 02:10:38 -0400

    implementação exemplo de redes

---
+++ REDES_PROTOCOLOS_TCP_UDP/src/tcp/TCPClient.java
@@ -0,0 +1,49 @@
+package tcp;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class TCPClient {
+    public static void main(String[] args) throws IOException {
+        
+        System.out.println("==CLIENT STARTED==");
+        
+        String sentence;
+        String modifiedSentence;
+        
+        do{
+            //cria stream de entrada
+            BufferedReader inFromUser = 
+                    new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("\nINPUT TEXT: ");
+            sentence = inFromUser.readLine(); 
+            
+            if(sentence.equalsIgnoreCase("EXIT")) break;
+            
+            //cria socket, conecta ao servidor
+            Socket clientSocket = new Socket("localhost", 6789);
+
+            //cria stream de saída ligado ao socket
+            DataOutputStream outToServer =
+                    new DataOutputStream(clientSocket.getOutputStream());
+
+            //envia linha para o servidor
+            outToServer.writeBytes(sentence + '\n');
+            
+            //cria stream de entrada ligado ao socket
+            BufferedReader inFromServer =  new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+
+            modifiedSentence = inFromServer.readLine();
+
+            System.out.println("OUTPUT FROM SERVER: " + modifiedSentence);
+            
+            clientSocket.close(); 
+
+        }while(!sentence.equalsIgnoreCase("EXIT"));
+    }
+}
---
+++ REDES_PROTOCOLOS_TCP_UDP/src/tcp/TCPServer.java
@@ -0,0 +1,57 @@
+package tcp;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("==SERVER STARTED==");
+        String clientSentence;
+        String capitalizedSentence;
+        
+        //cria socket de aceitação na porta 6789
+        ServerSocket welcomeSocket = new ServerSocket(6789);
+        
+        while(true) {
+            
+            System.out.println("\nwaiting interaction..");
+            
+            //espera, no socket de aceitação por contato do cliente
+            Socket connectionSocket = welcomeSocket.accept(); 
+            
+            System.out.println("interaction detected..");
+            
+            //cria stream de entrada, ligado ao socket
+            BufferedReader inFromClient =
+                new BufferedReader(
+                    new InputStreamReader(
+                        connectionSocket.getInputStream()));
+            
+            //cria stream de saída, ligado ao socket
+            DataOutputStream outToClient =
+                new DataOutputStream(
+                        connectionSocket.getOutputStream());
+            
+            //lê linha do socket
+            clientSentence = inFromClient.readLine();
+            
+            System.out.println("sentence received..");
+            
+            System.out.println("processing..");
+
+            capitalizedSentence = clientSentence.toUpperCase() + '\n';
+            
+            //escreve linha para o socket
+            outToClient.writeBytes(capitalizedSentence); 
+            
+            System.out.println();
+            
+        }//fim do while loop, retorne e espere por outra conexão do cliente
+    }
+    
+}
